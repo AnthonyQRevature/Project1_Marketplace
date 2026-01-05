5812b00eb963bfdcdf2cd12072481d2d0173f040
feyza <feyzamacan@gmail.com>
2026-01-05 13:25:12 -0600

    hi

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,13 @@
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id SERIAL PRIMARY KEY
-);
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    role VARCHAR(10) DEFAULT 'USER',
+    verified_seller BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
 
 CREATE TABLE IF NOT EXISTS user_profiles
 (
