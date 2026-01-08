4422fa7fc1fca65cba4d3d09dbb0cd1a74e56bc0
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2026-01-08 13:36:20 -0600

    added react router

---
+++ marketplace_app/src/Test.tsx
@@ -0,0 +1,11 @@
+
+function Test()
+{
+  return (
+      <>
+        <p>This is a test</p>
+      </>
+  );
+}
+
+export default Test;
--- marketplace_app/src/main.tsx
+++ marketplace_app/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router"; //router
+import './index.css';
+import App from './App.tsx';
+import Test from './Test.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/route" element={<Test />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
