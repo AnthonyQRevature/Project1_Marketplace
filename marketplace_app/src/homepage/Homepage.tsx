3c2c6ed2def4f75341c574c051cb36620e2d4a0e
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2026-01-12 11:10:29 -0600

    Prototype of the Login Page (needs testing once Login is done

--- marketplace_app/src/homepage/Homepage.tsx
+++ marketplace_app/src/homepage/Homepage.tsx
@@ -9,7 +9,7 @@ function Homepage()
       <Head />
       <div className="cards">
         <div className="card">
-          <Link to="/register"><p>Register</p></Link>
+          <Link to="/login"><p>Login</p></Link>
         </div>
         <div className="card">
           <p>Content</p>
--- marketplace_app/src/main.tsx
+++ marketplace_app/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router"; //router
+import { BrowserRouter, Routes, Route } from "react-router"; //React Router
 import './index.css';
 import Homepage from './homepage/Homepage.tsx';
 import Test from './Test.tsx';
-import Register from './register_user/register.tsx';
+import Register from './register_user/Register.tsx';
+import Login from './login/login.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/test" element={<Test />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
