2cc83acd839c5ada15e402a9995d704dc2d2e9ee
Baconboss92 <murphy@keltor.org>
2026-01-13 12:22:30 -0600

    Stand in methods

---
+++ marketplace/src/main/java/project/controller/model/UserProfileModel.java
@@ -0,0 +1,5 @@
+package project.controller.model;
+
+public class UserProfileModel {
+    
+}
--- marketplace/src/main/java/project/service/UserProfileService.java
+++ marketplace/src/main/java/project/service/UserProfileService.java
@@ -2,8 +2,32 @@ package project.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+import project.controller.model.UserProfileModel;
+
 @Service
 public class UserProfileService{
     int a;
-    Optional<UserProfileModel> get
+    Optional<UserProfileModel> getProfileModelByUserID(int userID){
+    }
+    Optional<UserProfileModel> updateUserProfile(UserProfileModel model){
+    }
+    UserProfileEntity modelToEntity(UserProfileModel model){
+    }
+    UserProfileModel entityToModel(UserProfileEntity entity){
+    }
+    boolean deleteUserProfileByID(int id){
+    }
+    boolean deleteUserProfileByUserID(int userID){
+    }
+    boolean deleteUserProfileByModel(UserProfileModel model){
+    }
+    boolean deleteUserProfileByEntity(UserProfileEntity entity){
+    }
+
+    Optional<UserProfileModel> createNewUserProfile(UserProfileModel model){
+    }
+    boolean uniqueUserID(int userID){
+    }
 }
