09e51de9a897596558eb472e0275bfa4103da880
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2026-01-20 17:36:25 -0600

    get users by distance

---
+++ database/fn.sql
@@ -0,0 +1,18 @@
+CREATE FUNCTION radians(DOUBLE PRECISION) RETURNS DOUBLE PRECISION AS $$
+    SELECT $1 * PI() / 180;
+$$ LANGUAGE SQL
+
+CREATE FUNCTION haversine_formula(lat1 DOUBLE PRECISION, lon1 DOUBLE PRECISION, lat2 DOUBLE PRECISION, lon2 DOUBLE PRECISION) RETURNS DOUBLE PRECISION AS $$
+    SELECT 2*ear_rad*ASIN(SQRT(s1 * s1 + COS(radians(lat1)) * COS(radians(lat2)) * s2 * s2)) FROM (
+        SELECT 
+            SIN((radians(lat2) - radians(lat1)) / 2) AS s1, 
+            SIN((radians(lon2) - radians(lon1)) / 2) AS s2, 
+            3959 AS ear_rad
+    );
+$$ LANGUAGE SQL
+
+CREATE VIEW user_distances AS(
+    SELECT users.id, users.username, user_profile.pfp_encoded, user_profile.latitude, user_profile.longitude
+    FROM users INNER JOIN user_profile ON users.id = user_profile.id
+
+);
