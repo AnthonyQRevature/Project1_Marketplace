1a19350e94f9bca74e56931a95d2300a757f556a
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2026-01-24 12:20:45 -0600

    fixed security and data type redundancies

--- marketplace/src/main/java/project/controller/BlocksController.java
+++ marketplace/src/main/java/project/controller/BlocksController.java
@@ -3,16 +3,19 @@ package project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
-import project.controller.Bodies.BlockedBy;
+import project.controller.Bodies.BlockBody;
 import project.service.BlocksService;
 import project.util.AllowCORS;
-import project.util.SecureIndescriminate;
+import project.util.Secure;
 import project.util.SecurityLevel;
-import project.controller.Bodies.BlockBody;
-
-import java.util.List;
 
 @RestController
 @AllowCORS
@@ -25,29 +28,33 @@ public class BlocksController {
 	}
 
 	@GetMapping("/users/{id}/blocks")
-	@SecureIndescriminate(SecurityLevel.ADMIN)
-	public ResponseEntity getAllBlocksBy(
+	@Secure(SecurityLevel.USER)
+	public ResponseEntity<?> getAllBlocksBy(
 			@RequestHeader("Authorization") String authHeader,
-			@RequestBody BlockedBy blockedBy)
-	{
-		return ResponseEntity.ok(blocksService.getAllBy(blockedBy.getId_blocker()));
+			@PathVariable int id
+	) {
+		return ResponseEntity.ok(blocksService.getAllBy(id));
 	}
 
 	@PostMapping("/users/{id}/blocks")
-	@SecureIndescriminate(SecurityLevel.ADMIN)
-	public ResponseEntity insertBlock(	@RequestHeader("Authorization") String authHeader,
-								@RequestBody BlockBody body)
+	@Secure(SecurityLevel.USER)
+	public ResponseEntity<?> insertBlock(	
+		@RequestHeader("Authorization") String authHeader,
+		@PathVariable int id,
+		@RequestBody BlockBody body)
 	{
-		blocksService.insertBlock(body.getId_blocker(), body.getId_blocked());
+		blocksService.insertBlock(id, body.getId_blocked());
 		return ResponseEntity.ok().build();
 	}
 
 	@DeleteMapping("/users/{id}/blocks")
-	@SecureIndescriminate(SecurityLevel.USER)
-	public ResponseEntity unBlock(@RequestHeader("Authorization") String authHeader,
-								  @RequestBody BlockBody body, Errors error)
+	@Secure(SecurityLevel.USER)
+	public ResponseEntity<?> unBlock(
+		@RequestHeader("Authorization") String authHeader,
+		@PathVariable int id,
+		@RequestBody BlockBody body, Errors error)
 	{
-		blocksService.delete(body.getId_blocker(), body.getId_blocked());
+		blocksService.delete(id, body.getId_blocked());
 		return ResponseEntity.ok().build();
 	}
 }
