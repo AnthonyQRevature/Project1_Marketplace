115c75b9aa544d1b9811fb1a1cf17caed471447a
LiquidEnder <ajhughes2000@icloud.com>
2026-01-23 22:31:41 -0600

    changed the navBar so that when you click Secondhand, it takes you to the Homepage

--- marketplace_app/src/NavBar.tsx
+++ marketplace_app/src/NavBar.tsx
@@ -10,7 +10,7 @@ export default function NavBar()
   return (
     <div className="nav_bar">
       <div></div>
-      <h1 className="title">Secondhand</h1>
+      <h1 className="title"><Link to={`/`}><p>Secondhand</p></Link></h1>
       <MyProfile auth={auth}/>
     </div>
   )
