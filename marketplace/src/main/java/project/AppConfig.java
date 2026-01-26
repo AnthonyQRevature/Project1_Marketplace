8b857ad6bcc253b62cf944291d040e789dd105e0
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2026-01-26 12:59:13 -0600

    Application security (this might break stuff)

--- marketplace/src/main/java/project/AppConfig.java
+++ marketplace/src/main/java/project/AppConfig.java
@@ -5,9 +5,10 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 
 import project.properties.MediaProperties;
+import project.util.TokenUtil;
 
 @SpringBootApplication
-@EnableConfigurationProperties(MediaProperties.class)
+@EnableConfigurationProperties({MediaProperties.class, TokenUtil.TokenProperties.class})
 public class AppConfig {
 
     @Bean
