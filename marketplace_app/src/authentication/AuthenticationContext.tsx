87acf226bcf0327a388298dba57ee73f867edfe6
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2026-01-27 11:14:09 -0600

    pulled dev branch's authentication context

--- marketplace_app/src/authentication/AuthenticationContext.tsx
+++ marketplace_app/src/authentication/AuthenticationContext.tsx
@@ -2,20 +2,43 @@ import { createContext, useState, type Dispatch } from "react";
 import type { JSX } from "react/jsx-runtime";
 
 //this stuff is complicated enough to be wrapped in a class
-export type Authentication = 
-{
-  userId: number
-  username: string
-  token: string
-} | {};
+export class Authentication {
+  id = -1;
+  username = "guest";
+  encryptedToken = "";
+  role = 0; // guest
+
+  constructor(init? : {id: number, role: number, username: string, encryptedToken: string})
+  {
+    if (init)
+    {
+      this.id = init.id;
+      this.username = init.username;
+      this.encryptedToken = init.encryptedToken;
+      this.role = init.role;
+    }
+    else
+    {
+      
+    }
+  }
+
+  isGuest() {
+    return this.id === -1;
+  }
+
+  isAdmin() {
+    return this.role === 2;
+  }
+};
 
 export type AuthenticationState = [Authentication, Dispatch<React.SetStateAction<Authentication>>];
 
-const AuthenticationContext = createContext<AuthenticationState>([{}, (_x)=>{}]);
+const AuthenticationContext = createContext<AuthenticationState>([new Authentication(), (_x)=>{}]);
 
-export function AuthenticationProvider(props : {children: JSX.Element})
+export function AuthenticationProvider(props : {children: JSX.Element[] | JSX.Element})
 {
-  const [auth, setAuth] = useState<Authentication>({});
+  const [auth, setAuth] = useState<Authentication>(new Authentication());
   
   return (
     <AuthenticationContext value={[auth, setAuth]}>
