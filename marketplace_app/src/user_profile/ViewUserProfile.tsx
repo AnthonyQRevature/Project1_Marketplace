d6cc7b826847e7aea2af9e62f82353811dbd8443
Baconboss92 <murphy@keltor.org>
2026-01-27 17:50:59 -0600

    Fixing git pull issues

--- marketplace_app/src/user_profile/ViewUserProfile.tsx
+++ marketplace_app/src/user_profile/ViewUserProfile.tsx
@@ -1,11 +1,15 @@
-import { createContext, useContext, useState, useEffect } from "react";
-import "./ViewUserProfile.css";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Link, useNavigate, useParams, type NavigateFunction } from "react-router";
 import AuthenticationContext, { Authentication, type AuthenticationState } from "../authentication/AuthenticationContext";
 import { EncodedImage } from "../util/EncodedImage";
 import type { Endpoint } from "../util/Endpoint";
 import DeleteButton from "./DeleteButton";
 import useLoader from "../util/AsyncLoader";
+import type { UserProfile } from "../util/DataStructure";
+import ProfileSocialButtons from "./SocialButtons";
+
+import mailIcon from "../assets/mail.png";
+import "./ViewUserProfile.css";
 
 //get user profile endpoint
 function makeEndpoint(id: number) : Endpoint
@@ -17,30 +21,19 @@ function pfp_endpoint(id: number) : Endpoint
   return {endpoint: `http://localhost:8080/users/${id}/media`, method: "POST"};
 }
 
-const API_BASE = "http://localhost:8080";
-
-type UserProfile = {
-  id : number,
-  username : string,
-  email : string,
-  profile: 
-  {
-    pfp_encoded : string,
-    bio : string,
-    latitude : number,
-    longitude : number,
-    distance : number
-  }
-}
-
-function ViewUserProfile()
+function ViewUserProfilePage()
 {
   const {user_id} = useParams();
-  const [auth, _] = useContext(AuthenticationContext);
   const [AsyncLoader, reset] = useLoader<UserProfile>(makeEndpoint(Number(user_id)));
 
+  //reload on param change
+  useEffect(reset, [user_id]);
+
   return (
-    <AsyncLoader foward={{reset: reset}} then={Display} otherwise={() => <p>Loading</p>}/>
+    <>
+      <AsyncLoader foward={{reset: reset}} then={Display} otherwise={() => <p>Loading</p>}/>
+      <Attribution />
+    </>
   );
 }
 
@@ -57,26 +50,26 @@ function Display(props : {resource : UserProfile, reset: () => void})
   {
     owner = true;
   }
-
+  
   const fetchStatus = async () => {
     try {
-      const response = await fetch(`${API_BASE}/users/ADMIN`, {
-        method: "GET",
-        headers: {
-          Authorization: auth.encryptedToken
+        const response = await fetch(`http://localhost:8080/users/ADMIN`, {
+            method: "GET",
+            headers: {
+                Authorization: auth.encryptedToken
+            }
+        });
+        
+        if (response.ok) {
+            setAdmin(true);
+        } else {
+            setAdmin(false);
         }
-      });
-
-      if (response.ok) {
-        setAdmin(true);
-      } else {
-        setAdmin(false);
-      }
     } catch (e) {
-      console.error(e);
+        console.error(e);
     }
   };
-  
+
   useEffect(() => {
     fetchStatus();
   }, []);
@@ -85,15 +78,24 @@ function Display(props : {resource : UserProfile, reset: () => void})
   {
     return (
       <>
-        <EncodedImage img={profile.profile.pfp_encoded} />
-        {owner? <button onClick={()=>setEditMode(true)}>edit</button> : <></>}
-        <h1>{profile.username}</h1>
-        <p>{profile.email}</p>
-        <p>BIO:</p>
-        <p>{profile.profile.bio}</p>
-        <p>Location: {profile.profile.latitude}, {profile.profile.longitude}</p>
-        {admin && (<Link to={`/users/${profile.id}/reports/of`}><p>Reports Against </p></Link>)}
-        {(admin || owner) && (<Link to={`/users/${profile.id}/reports/from`}><p>Reports Made </p></Link>)}
+        {owner? <InboxIcon /> : <MsgIcon /> }
+        {auth.isAdmin()? <DeleteButton /> : <></>}
+        <div className="profile-columns">
+          <div>
+            <EncodedImage img={profile.profile.pfp_encoded} />
+            {owner? <button onClick={()=>setEditMode(true)}>edit</button> : <></>}
+            <h1>{profile.username}</h1>
+            <p>{profile.email}</p>
+            <p>BIO:</p>
+            <p>{profile.profile.bio}</p>
+            <p>Location: {profile.profile.latitude}, {profile.profile.longitude}</p>
+            {admin && (<Link to={`/users/${profile.id}/reports/of`}><p>Reports Against </p></Link>)}
+            {(admin || owner) && (<Link to={`/users/${profile.id}/reports/from`}><p>Reports Made </p></Link>)}
+          </div>
+          <div>
+            <ProfileSocialButtons user_id={Number(user_id)}/>
+          </div>
+        </div>
       </>
     );
   }
@@ -187,4 +189,30 @@ function submitProfile(token : string, profile_endpoint : Endpoint, pfp_endpoint
   }
 }
 
-export default ViewUserProfile;
+function InboxIcon()
+{
+  const nav = useNavigate();
+
+  return (
+    <img className="icon link" src={mailIcon} onClick={() => nav("/inbox")} />
+  );
+}
+
+function MsgIcon()
+{
+  const nav = useNavigate();
+  const {user_id} = useParams();
+
+  return (
+    <img className="icon link" src={mailIcon} onClick={() => nav(`/message/${user_id}`)} />
+  );
+}
+
+function Attribution()
+{
+  return (
+    <a className="attribution" href="https://www.flaticon.com/free-icons/email" title="email icons">Email icon created by Freepik - Flaticon</a>
+  );
+}
+
+export default ViewUserProfilePage;
--- marketplace_app/src/util/AsyncLoader.tsx
+++ marketplace_app/src/util/AsyncLoader.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState, type FC } from "react";
+import { useContext, useEffect, useState, type FC } from "react";
 import type { Endpoint } from "./Endpoint";
 import AsyncLoader from "./AsyncLoaderPlain";
 import logError from "./logError";
+import AuthenticationContext from "../authentication/AuthenticationContext";
 
-type AsyncLoaderInit<T> = 
+type AsyncLoaderInit = 
 {
   foward? : any,
   then : FC<any>, 
@@ -11,9 +12,12 @@ type AsyncLoaderInit<T> =
   abort? : FC
 };
 
-type AsyncLoader<T> = FC<AsyncLoaderInit<T>>;
+type AsyncLoader = FC<AsyncLoaderInit>;
 
-export default function useLoader<T>(endpoint : Endpoint, repititions : number = 5) : [AsyncLoader<T>, () => void]
+//this thing isnt perfect
+//if i had a better understanding of typescript i could fix the lack of type safety
+//the loader state is shared with the data when they should be seperate
+export default function useLoader<T>(endpoint : Endpoint, repititions : number = 5) : [AsyncLoader, () => void]
 {
   const LOADER_STATE = 
   {
@@ -28,21 +32,31 @@ export default function useLoader<T>(endpoint : Endpoint, repititions : number =
     setState(LOADER_STATE.LOADING);
   }
   
-  const LoaderComponent = (props : AsyncLoaderInit<T>) =>
+  const LoaderComponent = (props : AsyncLoaderInit) =>
   {
     let Then = props.then;
     let Otherwise : FC = props.otherwise || (() => <></>);
     let Abort : FC = props.abort || props.otherwise || (() => <></>);
+
+    const [auth, _] = useContext(AuthenticationContext);
 
     //run fetch up to rep times
     const async_fetch = async (rep: number) => {
       let i = 0;
       while (true) {
         try {
-          const response = await fetch(endpoint.endpoint, { method: endpoint.method });
-          const value: T = await response.json();
-          setState(value);
-          return;
+          const response = await fetch(endpoint.endpoint, { 
+            method: endpoint.method,
+            headers: {
+              Authorization: auth.encryptedToken
+            }
+          });
+          if (response.ok)
+          {
+            const value: T = await response.json();
+            setState(value);
+            return;
+          }
         }
         catch (e) {
           //fail
@@ -68,11 +82,11 @@ export default function useLoader<T>(endpoint : Endpoint, repititions : number =
       async_fetch(repititions)
     }, [currentState]);
 
-    if (currentState == LOADER_STATE.LOADING)
+    if (currentState === LOADER_STATE.LOADING)
     {
       return (<Otherwise />);
     }
-    else if (currentState == LOADER_STATE.ABORTED)
+    else if (currentState === LOADER_STATE.ABORTED)
     {
       return (<Abort />);
     }
@@ -81,6 +95,115 @@ export default function useLoader<T>(endpoint : Endpoint, repititions : number =
       //loaded the resource
       const resource = currentState as T;
       return (<Then {...{resource: resource, ...props.foward}}/>);
+    }
+  }
+  return [LoaderComponent, resetFunction];
+}
+
+export function useLoaderses<T>(endpoints : Endpoint[], repititions : number = 5) : [AsyncLoader, () => void]
+{
+  const LOADER_STATE = 
+  {
+    LOADING: 0,
+    ABORTED: 1,
+    DONE: 2
+  }
+  
+  const [currentState, setState] = useState<number>(LOADER_STATE.LOADING);
+  const [currentData, setData] = useState<T[] | null>(null);
+
+  const resetFunction = () => 
+  {
+    setState(LOADER_STATE.LOADING);
+  }
+  
+  const LoaderComponent = (props : AsyncLoaderInit) =>
+  {
+    let Then = props.then;
+    let Otherwise : FC = props.otherwise || (() => <></>);
+    let Abort : FC = props.abort || props.otherwise || (() => <></>);
+
+    const [auth, _] = useContext(AuthenticationContext);
+
+    //run fetch up to rep times
+    const async_fetch = async (rep: number, endpoint : Endpoint) => {
+      let i = 0;
+      while (true) {
+        try {
+          const response = await fetch(endpoint.endpoint, { 
+            method: endpoint.method,
+            headers: {
+              Authorization: auth.encryptedToken
+            }
+          });
+          if (response.ok)
+          {
+            const value: T = await response.json();
+            return value;
+          }
+        }
+        catch (e) {
+          //fail
+          logError(e);
+        }
+  
+        //failed
+        i++;
+        if (i < rep) {
+          logError("Fetch request failed. Retrying.");
+        }
+        else {
+          logError("Fetch request failed. Too many attempts. Aborting.")
+          break;
+        }
+      }
+      return LOADER_STATE.ABORTED;
+    };
+
+    //run whenever currentState is set to LOADING
+    useEffect(() => {
+      let runner = async () => {
+        let aborted = false;
+        if (currentState !== LOADER_STATE.LOADING) return;
+        
+        let result = new Array<T>(endpoints.length);
+        let promises = endpoints.map((endpoint) => async_fetch(repititions, endpoint));
+
+        for (let i = 0; i < result.length; i++)
+        {
+          let val = await promises[i];
+          if (val !== LOADER_STATE.ABORTED)
+            result[i] = val as T;
+          else
+          {
+            //if one of the operations aborts then we set the loader state to aborted (maybe keep going as to not waste the fetch request)
+            aborted = true;
+          }
+        }
+        
+        setData(result);
+        if (!aborted)
+          setState(LOADER_STATE.DONE);
+        else
+          setState(LOADER_STATE.ABORTED);
+      };
+      runner();
+    }, [currentState]);
+
+    //only resort to Otherwise if there is no data present
+    if (currentState === LOADER_STATE.LOADING && currentData == null)
+    {
+      return (<Otherwise />);
+    }
+    else if (currentState === LOADER_STATE.ABORTED)
+    {
+      return (<Abort />);
+    }
+    else
+    {
+      //loaded the resource
+      const resources = currentData as T[];
+      return (<Then {...{resources: resources, ...props.foward}}/>);
     }
   }
   return [LoaderComponent, resetFunction];
