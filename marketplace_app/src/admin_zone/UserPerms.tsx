adbc011cbaf8173538d54db0d259351a646dd643
anthony17mai <43441412+anthony17mai@users.noreply.github.com>
2026-01-31 16:31:56 -0600

    change role from front end

--- marketplace_app/src/admin_zone/UserPerms.tsx
+++ marketplace_app/src/admin_zone/UserPerms.tsx
@@ -1,14 +1,23 @@
-import { useContext, useEffect, useState, type ChangeEvent } from "react";
+import { useContext, useEffect, useState, type ChangeEvent, type ChangeEventHandler } from "react";
 import { Role, type UserProfile } from "../util/DataStructure";
 import AuthenticationContext from "../authentication/AuthenticationContext";
 import { useNavigate } from "react-router";
 import useLoader from "../util/AsyncLoader";
 import { UserBrief } from "../util/UserBrief";
 import type { State } from "../util/State";
+import type { Endpoint } from "../util/Endpoint";
 
 const endpoint = {
   endpoint: "http://localhost:8080/users",
   method: "GET"
+}
+
+function makePermEndpoint(user_id : number) : Endpoint
+{
+  return {
+    endpoint: `http://localhost:8080/users/${user_id}/perms`,
+    method: "PATCH"
+  }
 }
 
 export default function UserPermsPage()
@@ -16,7 +25,7 @@ export default function UserPermsPage()
   const [auth,] = useContext(AuthenticationContext);
   const nav = useNavigate();
   const [AsyncLoader, reset] = useLoader(endpoint);
-  const [mod, setMod] = useState<any>({});
+  const [mod, setMod] = useState<Record<number, number>>({});
   useEffect(() => {auth.isAdmin() || nav("/login");}, [auth]);
   
   if (!auth.isAdmin())
@@ -26,15 +35,15 @@ export default function UserPermsPage()
 
   return (
     <>
-      <AsyncLoader then={UserList} foward={{modState: [mod, setMod]}}/>
+      <AsyncLoader then={UserList} foward={{modState: [mod, setMod], reset: reset}}/>
     </>
   );
 }
 
-function UserList(props : {resource : UserProfile[], modState : State<any>})
+function UserList(props : {resource : UserProfile[], modState : State<Record<number, number>>, reset : ()=>void})
 {
   const [auth,] = useContext(AuthenticationContext);
-  const {resource, modState} = props;
+  const {resource, modState, reset} = props;
   const [mod, setMod] = modState;
   
   //bind user id
@@ -62,9 +71,27 @@ function UserList(props : {resource : UserProfile[], modState : State<any>})
     };
   }
 
-  const handleSave = function()
+  const handleSave = async function()
   {
-    
+    let requests = [];
+    for (const [key, val] of Object.entries(mod))
+    {
+      const endpoint = makePermEndpoint(key as unknown as number);
+      requests.push(fetch(endpoint.endpoint, {
+        method: endpoint.method,
+        headers:{
+          Authorization: auth.encryptedToken,
+          "Content-Type": "application/json"
+        },
+        body: val.toString()
+      }));
+    }
+    for (const r in requests)
+    {
+      await r;
+    }
+    setMod({});
+    reset();
   }
 
   return (
@@ -73,14 +100,37 @@ function UserList(props : {resource : UserProfile[], modState : State<any>})
       {resource.map(profile => 
         <div className="horizontal" key={profile.id}>
           <UserBrief resource={profile} >
-            <select onChange={handleChange(profile.id)} disabled={auth.role !== Role.SUPER_USER}>
-              <option value="user" selected={profile.role === Role.USER}>User</option>
-              <option value="admin" selected={profile.role === Role.ADMIN}>Admin</option>
-              <option value="super" selected={profile.role === Role.SUPER_USER}>Super User</option>
-            </select>
+            <ProfilePerms 
+              mod={mod} 
+              profile={profile} 
+              handleChange={handleChange(profile.id)} 
+              disabled={auth.role !== Role.SUPER_USER || auth.id === profile.id}
+            />
           </UserBrief>
         </div>
       )}
+    </>
+  );
+}
+
+function ProfilePerms(props : {
+  profile : UserProfile,
+  mod : Record<number, number>, 
+  handleChange : ChangeEventHandler<HTMLSelectElement>, 
+  disabled : boolean}
+) {
+  const {profile, mod, handleChange, disabled} = props;
+
+  let role = mod[profile.id] || profile.role;
+
+  return (
+    <>
+      <p className={mod[profile.id]? "" : "hidden"}>*</p>
+      <select onChange={handleChange} disabled={disabled}>
+        <option value="user" selected={role === Role.USER}>User</option>
+        <option value="admin" selected={role === Role.ADMIN}>Admin</option>
+        <option value="super" selected={role === Role.SUPER_USER}>Super User</option>
+      </select>
     </>
   );
 }
